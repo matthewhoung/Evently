dc7a33e662ec20ec5ef10da006765440fd84e0e9
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-11-15 12:11:16 +0800

    重構事件模組並新增基礎設施專案
    刪除了 `Evently.Modules.Events.Api` 專案，並移除了相關的專案引用。
    新增了 `Evently.Modules.Events.Infrastructure` 專案，並更新了相關的專案引用。
    在 `Evently.Api.csproj` 和 `Program.cs` 中，將 `Evently.Modules.Events.Api` 的專案引用替換為 `Evently.Modules.Events.Infrastructure`。
    在 `Evently.Modules.Events.Application.csproj` 中，新增了 `Dapper` 套件引用。
    重構了 `GetEventQueryHandler` 類別，使用 Dapper 來查詢資料庫。
    在 `EventsModule.cs` 中，將命名空間從 `Evently.Modules.Events.Api` 更改為 `Evently.Modules.Events.Infrastructure`，並新增了基礎設施相關的服務註冊。
    新增了 `AssemblyReference.cs` 用於引用 `Evently.Modules.Events.Application` 程式集。
    新增了 `IDbConnectionFactory` 介面及其實現 `DbConnectionFactory` 類別，使用 Npgsql 來建立資料庫連接。
    新增了 `EventRepository` 類別，實現 `IEventRepository` 介面，用於事件資料的存取。

--- src/Modules/Events/Evently.Modules.Events.Application/Events/GetEvent.cs
+++ src/Modules/Events/Evently.Modules.Events.Application/Events/GetEvent.cs
@@ -1,30 +1,41 @@
-﻿using MediatR;
+﻿using System.Data.Common;
+using Dapper;
+using Evently.Modules.Events.Application.Abstractions.Data;
+using MediatR;
 
 namespace Evently.Modules.Events.Application.Events;
 
 public sealed record GetEventQuery(Guid EventId) : IRequest<EventResponse?>;
-public static class GetEvent
-{
-    public static void MapEndpoint(IEndpointRouteBuilder app)
-    {
-        app.MapGet("events/{id}", async (Guid id, EventsDbContext context) =>
-        {
-            EventResponse? @event = await context.Events
-                .Where(e => e.Id == id)
-                .Select(e => new EventResponse(
-                    e.Id,
-                    e.Title,
-                    e.Description,
-                    e.Location,
-                    e.StartsAtUtc,
-                    e.EndsAtUtc
-                ))
-                .SingleOrDefaultAsync();
 
-            return @event is null
-                    ? Results.NotFound()
-                    : Results.Ok(@event);
-        })
-            .WithTags(Tags.Events);
+internal sealed class GetEventQueryHandler(IDbConnectionFactory dbConnectionFactory)
+    : IRequestHandler<GetEventQuery, EventResponse?>
+{
+    public async Task<EventResponse?> Handle(GetEventQuery request, CancellationToken cancellationToken)
+    {
+        await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
+
+        const string sql =
+        $"""
+         SELECT
+             e.id AS {nameof(EventResponse.Id)},
+             e.category_id AS {nameof(EventResponse.CategoryId)},
+             e.title AS {nameof(EventResponse.Title)},
+             e.description AS {nameof(EventResponse.Description)},
+             e.location AS {nameof(EventResponse.Location)},
+             e.starts_at_utc AS {nameof(EventResponse.StartsAtUtc)},
+             e.ends_at_utc AS {nameof(EventResponse.EndsAtUtc)},
+             tt.id AS {nameof(TicketTypeResponse.TicketTypeId)},
+             tt.name AS {nameof(TicketTypeResponse.Name)},
+             tt.price AS {nameof(TicketTypeResponse.Price)},
+             tt.currency AS {nameof(TicketTypeResponse.Currency)},
+             tt.quantity AS {nameof(TicketTypeResponse.Quantity)}
+         FROM events.events e
+         LEFT JOIN events.ticket_types tt ON tt.event_id = e.id
+         WHERE e.id = @EventId
+         """;
+
+        EventResponse? @event = await connection.QueryFirstOrDefaultAsync(sql, request);
+
+        return @event;
     }
 }
--- src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
@@ -1,12 +1,18 @@
-﻿using Evently.Modules.Events.Api.Database;
+﻿using Evently.Modules.Events.Application.Abstractions.Data;
+using Evently.Modules.Events.Domain.Events;
+using Evently.Modules.Events.Infrastructure.Data;
+using Evently.Modules.Events.Infrastructure.Database;
+using Evently.Modules.Events.Infrastructure.Events;
 using Evently.Modules.Events.Presentation.Events;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Npgsql;
 
-namespace Evently.Modules.Events.Api;
+namespace Evently.Modules.Events.Infrastructure;
 public static class EventsModule
 {
     public static void MapEndpoints(IEndpointRouteBuilder app)
@@ -20,7 +26,26 @@ public static class EventsModule
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly);
+        });
+
+        services.AddInfrastructure(configuration);
+
+        return services;
+    }
+
+    private static void AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
         string databaseConnectionString = configuration.GetConnectionString("Database")!;
+        // NpgsqlDataSource will only registered onece, if it doesn't exist
+        NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
+        services.TryAddSingleton(npgsqlDataSource);
+        // Register IDbConnectionFactory for Dapper
+        services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
 
         services.AddDbContext<EventsDbContext>(options =>
             options
@@ -30,6 +55,8 @@ public static class EventsModule
                         .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Events))
                 .UseSnakeCaseNamingConvention());
 
-        return services;
+        services.AddScoped<IEventRepository, EventRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<EventsDbContext>());
     }
 }
