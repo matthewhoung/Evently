b4ad2f980d0f40632b4122a9069b555bab438ba1
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-11-20 16:19:33 +0800

    實作 IUnitOfWork 介面並新增 UserRepository 類別
    新增了一個命名空間 `Evently.Modules.Users.Application.Abstractions.Data`，並在其中定義了一個 `IUnitOfWork` 介面，包含一個 `SaveChangesAsync` 方法。
    在 `Evently.Modules.Users.Infrastructure.Database` 命名空間中新增了一個靜態類別 `Schemas`，並定義了一個常數 `Users`，其值為 "users"。
    在 `UserDbContext` 類別中實作了 `IUnitOfWork` 介面，並設定了預設的資料庫結構為 `Schemas.Users`，同時應用了 `UserConfiguration` 的設定。
    新增了一個 `UserConfiguration` 類別，實作了 `IEntityTypeConfiguration<User>` 介面，並在其中定義了 `User` 實體的資料庫結構，包括主鍵、屬性長度限制及唯一索引。
    新增了一個 `UserRepository` 類別，實作了 `IUserRepository` 介面，並提供了 `GetAsync` 和 `Insert` 方法，用於從資料庫中獲取和插入 `User` 實體。

---
+++ src/Modules/Users/Evently.Modules.Users.Infrastructure/Users/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Evently.Modules.Users.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Evently.Modules.Users.Infrastructure.Users;
+internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(key => key.Id);
+
+        builder.Property(fn => fn.FirstName).HasMaxLength(50);
+        builder.Property(ln => ln.LastName).HasMaxLength(50);
+        builder.Property(e => e.Email).HasMaxLength(100);
+        builder.HasIndex(e => e.Email).IsUnique();
+    }
+}
---
+++ src/Modules/Users/Evently.Modules.Users.Infrastructure/Users/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Evently.Modules.Users.Domain.Users;
+using Evently.Modules.Users.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Modules.Users.Infrastructure.Users;
+internal sealed class UserRepository(UserDbContext context) : IUserRepository
+{
+    public async Task<User?> GetAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Users.SingleOrDefaultAsync(user => user.Id == id, cancellationToken);
+    }
+
+    public void Insert(User user)
+    {
+        context.Users.Add(user);
+    }
+}
