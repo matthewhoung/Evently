28da052f4b7714b457983b11f4f0bd6f3352be9e
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-11-21 15:14:57 +0800

    新增 MassTransit 套件及使用者註冊相關功能
    在 `Evently.Common.Presentation.csproj` 中，新增了 `MassTransit.Abstractions` 套件引用，版本為 `8.3.2`。
    
    在 `Evently.Modules.Ticketing.Presentation.csproj` 中，新增了對 `Evently.Modules.Users.IntegrationEvents.csproj` 的專案引用，並移除了 `Customers` 資料夾的引用。
    
    在 `Evently.Modules.Users.IntegrationEvents.csproj` 中，移除了 `TargetFramework`、`ImplicitUsings` 和 `Nullable` 屬性。
    
    新增了 `UserRegisteredIntegrationEventConsumer.cs` 檔案，該檔案定義了一個實作 `IConsumer<UserRegisterdIntegrationEvent>` 的類別。
    
    新增了 `UserRegisteredDomainEventHandler.cs` 檔案，該檔案定義了一個處理 `UserRegisteredDomainEvent` 的事件處理器，並在處理過程中發佈 `UserRegisteredIntegrationEvent`。
    
    新增了 `UserRegisteredIntegrationEvent.cs` 檔案，該檔案定義了一個整合事件，包含使用者註冊的相關資訊。

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Presentation/Customers/UserRegisteredIntegrationEventConsumer.cs
@@ -0,0 +1,7 @@
+﻿using Evently.Modules.Users.IntegrationEvents;
+using MassTransit;
+
+namespace Evently.Modules.Ticketing.Presentation.Customers;
+public sealed class UserRegisteredIntegrationEventConsumer : IConsumer<UserRegisterdIntegrationEvent>
+{
+}
