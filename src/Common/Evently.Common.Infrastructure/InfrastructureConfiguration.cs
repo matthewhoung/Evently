405fe16806c854571cb1ff36c801990ba407b0dc
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-11-21 14:48:58 +0800

    新增 EventBus 和 MassTransit 支援
    在 `InfrastructureConfiguration.cs` 中，新增了 `Evently.Common.Application.EventBus` 和 `MassTransit` 的引用，並在 `AddInfrastructure` 方法中新增了 `moduleConfigureConsumers` 參數。移除了 PostgreSQL 連接的註冊區塊，並在 Redis 連接的註冊區塊中移除了 `#region` 和 `#endregion` 標籤。新增了 `IEventBus` 和 `EventBus.EventBus` 的單例註冊，並新增 `AddMassTransit` 方法來配置 MassTransit，使用 In-Memory 傳輸。在 `EventBus.cs` 中，新增了 `Evently.Common.Application.EventBus` 和 `MassTransit` 的引用，並新增了一個 `EventBus` 類別，實現 `IEventBus` 介面，使用 `IBus` 來發布整合事件。

--- src/Common/Evently.Common.Infrastructure/InfrastructureConfiguration.cs
+++ src/Common/Evently.Common.Infrastructure/InfrastructureConfiguration.cs
@@ -1,10 +1,12 @@
 ﻿using Evently.Common.Application.Caching;
 using Evently.Common.Application.Clock;
 using Evently.Common.Application.Data;
+using Evently.Common.Application.EventBus;
 using Evently.Common.Infrastructure.Caching;
 using Evently.Common.Infrastructure.Clock;
 using Evently.Common.Infrastructure.Data;
 using Evently.Common.Infrastructure.Interceptors;
+using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
@@ -15,14 +17,13 @@ public static class InfrastructureConfiguration
 {
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
+        Action<IRegistrationConfigurator>[] moduleConfigureConsumers,
         string databaseConnectionString,
         string redisConnectionString)
     {
-        #region postgres connection register
         NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
         services.TryAddSingleton(npgsqlDataSource);
-        #endregion
-        #region redis connection register
+
         try
         {
             IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
@@ -35,12 +36,27 @@ public static class InfrastructureConfiguration
         {
             services.AddDistributedMemoryCache();
         }
-        #endregion
 
         services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
         services.TryAddSingleton<PublishDomainEventsInterceptor>();
         services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
         services.TryAddSingleton<ICacheService, CacheService>();
+        services.TryAddSingleton<IEventBus, EventBus.EventBus>();
+
+        services.AddMassTransit(configure =>
+        {
+            foreach (Action<IRegistrationConfigurator> configureConsumer in moduleConfigureConsumers)
+            {
+                configureConsumer(configure);
+            }
+
+            configure.SetKebabCaseEndpointNameFormatter();
+
+            configure.UsingInMemory((context, cfg) =>
+            {
+                cfg.ConfigureEndpoints(context);
+            });
+        });
 
         return services;
     }
