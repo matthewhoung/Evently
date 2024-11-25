8eb5008a9ab6af95dce306694744ce19b655fa10
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-11-25 13:53:31 +0800

    新增事件統計相關功能
    新增多個檔案以支援事件統計功能，包括：
    - `EventStatisticsResponse.cs`：定義事件統計資料的記錄類別。
    - `GetEventStatisticsQuery.cs`：定義查詢事件統計資料的記錄類別。
    - `GetEventStatisticsQueryHandler.cs`：處理事件統計查詢的處理器。
    - `AttendeeCheckedInDomainEventHandler.cs`：處理參加者簽到事件。
    - `DuplicateCheckInAttemptedDomainEventHandler.cs`：處理重複簽到嘗試事件。
    - `EventCreatedDomainEventHandler.cs`：處理事件創建事件。
    - `InvalidCheckInAttemptedDomainEventHandler.cs`：處理無效簽到嘗試事件。
    - `TicketCreatedDomainEventHandler.cs`：處理票券創建事件。
    - `GetEventStatistics.cs`：設定查詢事件統計資料的 API 端點。

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Presentation/EventStatistics/GetEventStatistics.cs
@@ -0,0 +1,24 @@
+﻿using Evently.Common.Domain.Abstractions.Results;
+using Evently.Common.Presentation.EndPoints;
+using Evently.Common.Presentation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Attendance.Presentation.EventStatistics;
+
+internal sealed class GetEventStatistics : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("event-statistics/{id}", async (Guid id, ISender sender) =>
+        {
+            Result<EventStatisticsResponse> result = await sender.Send(new GetEventStatisticsQuery(id));
+
+            return result.Match(Results.Ok, ApiResults.Problem);
+        })
+        .RequireAuthorization(Permissions.GetEventStatistics)
+        .WithTags(Tags.EventStatistics);
+    }
+}
